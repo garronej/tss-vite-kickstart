5d9a0b6445b69ccc8f275439b6175198efb0a07b
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-04-30 19:38:48 +0200

    Minimal

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { GlobalStyles } from "tss-react";
+import { useStyles } from "tss";
+import { Body } from "./Body";
+import { Body2 } from "./Body2";
+
+export function App() {
+
+  const { isDark, scrollbarStyles, theme } = useStyles();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles
+        styles={{
+          "html, body": {
+            "margin": "0",
+            "padding": "0",
+          },
+          "html": {
+            "colorScheme": isDark ? "dark" : "light",
+            "backgroundColor": theme.palette.background.default
+          },
+          "body": {
+            "scrollBehavior": "smooth",
+            ...scrollbarStyles
+          }
+        }}
+      />
+      <Body />
+      <Body2 />
     </>
   )
 }
 
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// This enables absolute imports like `import { useOidc } from "oidc";`
+// instead of `import { useOidc } from "../../oidc";`
+import tsconfigPaths from "vite-tsconfig-paths";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
